5bbc79fa097e18cbc5aa937d0c1e3002ace14bc8
Sam Hill <samuhill@gmail.com>
2021-11-21 09:23:20 -0500

    more charts

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -31,7 +31,7 @@ export const Stats = () => {
         .then(
           (result) => {
             setStats(result);
-            console.log(result)
+            console.log(result);
           },
           (error) => {
             console.error(error);
@@ -44,14 +44,16 @@ export const Stats = () => {
     return null;
   }
 
-  const statsSortedByDay = stats.sort((a, b) => moment(a.datetime) - moment(b.datetime));
+  const statsSortedByDay = stats.sort(
+    (a, b) => moment(a.datetime) - moment(b.datetime)
+  );
 
-  const lastUpdated = moment(statsSortedByDay[statsSortedByDay.length - 1].datetime);
-  const nextUpdate = moment(lastUpdated).add(24, "hours"); 
+  const lastUpdated = moment(
+    statsSortedByDay[statsSortedByDay.length - 1].datetime
+  );
+  const nextUpdate = lastUpdated.add(24, "hours");
 
-  const hoursUntilUpdate = moment.duration(
-    nextUpdate.diff(moment())
-  ).asHours();
+  const hoursUntilUpdate = moment.duration(nextUpdate.diff(moment())).asHours();
 
   const groupedStats = groupBy(stats, (stat) => {
     return moment(stat.datetime).startOf("day").format();
@@ -76,7 +78,6 @@ export const Stats = () => {
     )
     .sort((stat1, stat2) => moment(stat1.name) - moment(stat2.name));
 
-
   const coinAmounts = Object.values(groupedStats)
     .map((statsForDay) =>
       statsForDay
@@ -84,7 +85,7 @@ export const Stats = () => {
           name: moment(stat.datetime).format("MM/DD/YY"),
           actualCoinAmount: stat.actual_coin_amount,
           comparisonCoinAmount: stat.comparison_coin_amount,
-          ticker: stat.ticker
+          ticker: stat.ticker,
         }))
         .reduce(
           (prev, cur) => ({
@@ -98,64 +99,81 @@ export const Stats = () => {
     )
     .sort((stat1, stat2) => moment(stat1.name) - moment(stat2.name));
 
+  const dollarsSpent = Object.values(groupedStats)
+    .map((statsForDay) =>
+      statsForDay
+        .map((stat) => ({
+          name: moment(stat.datetime).format("MM/DD/YY"),
+          totalSpend: stat.total_spend,
+          ticker: stat.ticker,
+        }))
+        .reduce(
+          (prev, cur) => ({
+            ...prev,
+            name: cur.name,
+            [`${cur.ticker}TotalSpend`]: cur.totalSpend,
+          }),
+          {}
+        )
+    )
+    .sort((stat1, stat2) => moment(stat1.name) - moment(stat2.name));
 
-    const dollarsSpent = Object.values(groupedStats)
-      .map((statsForDay) =>
-        statsForDay
-          .map((stat) => ({
-            name: moment(stat.datetime).format("MM/DD/YY"),
-            totalSpend: stat.total_spend,
-            ticker: stat.ticker,
-          }))
-          .reduce(
-            (prev, cur) => ({
-              ...prev,
-              name: cur.name,
-              [`${cur.ticker}TotalSpend`]: cur.totalSpend,
-            }),
-            {}
-          )
-      )
-      .sort((stat1, stat2) => moment(stat1.name) - moment(stat2.name));
+  const marketPrice = Object.values(groupedStats)
+    .map((statsForDay) =>
+      statsForDay
+        .map((stat) => ({
+          name: moment(stat.datetime).format("MM/DD/YY"),
+          currentPrice: stat.current_price,
+          ticker: stat.ticker,
+        }))
+        .reduce(
+          (prev, cur) => ({
+            ...prev,
+            name: cur.name,
+            [`${cur.ticker}CurrentPrice`]: cur.currentPrice,
+          }),
+          {}
+        )
+    )
+    .sort((stat1, stat2) => moment(stat1.name) - moment(stat2.name));
 
+  const coinsPurchased = Object.values(groupedStats)
+    .map((statsForDay) =>
+      statsForDay
+        .map((stat) => ({
+          name: moment(stat.datetime).format("MM/DD/YY"),
+          actualCoinAmount: stat.actual_coin_amount,
+          ticker: stat.ticker,
+        }))
+        .reduce(
+          (prev, cur) => ({
+            ...prev,
+            name: cur.name,
+            [`${cur.ticker}CoinAmount`]: cur.actualCoinAmount,
+          }),
+          {}
+        )
+    )
+    .sort((stat1, stat2) => moment(stat1.name) - moment(stat2.name));
 
-    const coinsPurchased = Object.values(groupedStats)
-      .map((statsForDay) =>
-        statsForDay
-          .map((stat) => ({
-            name: moment(stat.datetime).format("MM/DD/YY"),
-            actualCoinAmount: stat.actual_coin_amount,
-            ticker: stat.ticker,
-          }))
-          .reduce(
-            (prev, cur) => ({
-              ...prev,
-              name: cur.name,
-              [`${cur.ticker}CoinAmount`]: cur.actualCoinAmount,
-            }),
-            {}
-          )
-      )
-      .sort((stat1, stat2) => moment(stat1.name) - moment(stat2.name));
-    
-    const dcaComparisonAveragePrice = Object.values(groupedStats)
-      .map((statsForDay) =>
-        statsForDay
-          .map((stat) => ({
-            name: moment(stat.datetime).format("MM/DD/YY"),
-            averageDcaPrice: stat.average_dca_price,
-            ticker: stat.ticker,
-          }))
-          .reduce(
-            (prev, cur) => ({
-              ...prev,
-              name: cur.name,
-              [`${cur.ticker}AverageDcaPrice`]: cur.averageDcaPrice,
-            }),
-            {}
-          )
-      )
-      .sort((stat1, stat2) => moment(stat1.name) - moment(stat2.name));
+  const dcaComparisonAveragePrice = Object.values(groupedStats)
+    .map((statsForDay) =>
+      statsForDay
+        .map((stat) => ({
+          name: moment(stat.datetime).format("MM/DD/YY"),
+          averageDcaPrice: stat.average_dca_price,
+          ticker: stat.ticker,
+        }))
+        .reduce(
+          (prev, cur) => ({
+            ...prev,
+            name: cur.name,
+            [`${cur.ticker}AverageDcaPrice`]: cur.averageDcaPrice,
+          }),
+          {}
+        )
+    )
+    .sort((stat1, stat2) => moment(stat1.name) - moment(stat2.name));
 
   const btcDollarsSpent =
     dollarsSpent[dollarsSpent.length - 1].btcusdTotalSpend;
@@ -177,6 +195,22 @@ export const Stats = () => {
   const ethDcaComparisonPrice =
     dcaComparisonAveragePrice[dcaComparisonAveragePrice.length - 1]
       .ethusdAverageDcaPrice;
+
+  const btcLatestPrice = parseFloat(
+    marketPrice[marketPrice.length - 1].btcusdCurrentPrice
+  );
+  const ethLatestPrice = parseFloat(
+    marketPrice[marketPrice.length - 1].ethusdCurrentPrice
+  );
+
+  const btcDcaComparisonCoinValue =
+    coinAmounts[coinAmounts.length - 1].btcusdComparison * btcLatestPrice;
+  const btcActualCoinValue =
+    coinAmounts[coinAmounts.length - 1].btcusdActual * btcLatestPrice;
+  const ethDcaComparisonCoinValue =
+    coinAmounts[coinAmounts.length - 1].ethusdComparison * ethLatestPrice;
+  const ethActualCoinValue =
+    coinAmounts[coinAmounts.length - 1].ethusdActual * ethLatestPrice;
 
   return (
     <>
@@ -219,7 +253,7 @@ export const Stats = () => {
           </Text>
         </Box>
       </Flex>
-      <Flex flexDirection={["column", "row"]}>
+      <Flex flexDirection={["column", "row"]} flexWrap="wrap">
         <Box width={[1, 1 / 3]} mt="2" mb="2">
           <Text as="h3">Bitcoin: price per coin comparison</Text>
           <ResponsiveContainer width="100%" height={400}>
@@ -334,35 +368,93 @@ export const Stats = () => {
             </BarChart>
           </ResponsiveContainer>
         </Box>
-      </Flex>
 
-      <Box width={[1, 1 / 3]} mt="2" mb="2">
-        <Text as="h3">Limit strategy advantage over time</Text>
-        <Text>(percentage dollars saved per coin)</Text>
-        <ResponsiveContainer width="100%" height={400}>
-          <LineChart
-            data={savingsPercentages}
-            margin={{ top: 5, right: 5, left: 10, bottom: 5 }}
-          >
-            <XAxis dataKey="name" stroke="#ebebeb" />
-            <Tooltip />
-            <Legend />
-            <CartesianGrid stroke="#333" />
-            <Line
-              type="monotone"
-              dataKey="btcusd"
-              stroke={COLORS.btc}
-              yAxisId={0}
-            />
-            <Line
-              type="monotone"
-              dataKey="ethusd"
-              stroke={COLORS.eth}
-              yAxisId={1}
-            />
-          </LineChart>
-        </ResponsiveContainer>
-      </Box>
+        <Box width={[1, 1 / 3]} mt="2" mb="2">
+          <Text as="h3">Bitcoin: coin value, in dollars</Text>
+          <ResponsiveContainer width="100%" height={400}>
+            <BarChart
+              data={[
+                {
+                  dca: btcDcaComparisonCoinValue,
+                  limit: btcActualCoinValue,
+                },
+              ]}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid stroke="#333" />
+              <XAxis dataKey="name" stroke="#ebebeb" />
+              <YAxis stroke="#ebebeb" />
+              <Tooltip />
+              <Legend />
+
+              <Bar dataKey="dca" name="dca strategy" fill={COLORS.comparison} />
+              <Bar dataKey="limit" name="limit strategy" fill={COLORS.btc} />
+            </BarChart>
+          </ResponsiveContainer>
+        </Box>
+
+        <Box width={[1, 1 / 3]} mt="2" mb="2">
+          <Text as="h3">Ethereum: coin value, in dollars</Text>
+          <ResponsiveContainer width="100%" height={400}>
+            <BarChart
+              data={[
+                {
+                  dca: ethDcaComparisonCoinValue,
+                  limit: ethActualCoinValue,
+                },
+              ]}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid stroke="#333" />
+              <XAxis dataKey="name" stroke="#ebebeb" />
+              <YAxis stroke="#ebebeb" />
+              <Tooltip />
+              <Legend />
+
+              <Bar dataKey="dca" name="dca strategy" fill={COLORS.comparison} />
+              <Bar dataKey="limit" name="limit strategy" fill={COLORS.eth} />
+            </BarChart>
+          </ResponsiveContainer>
+        </Box>
+
+        <Box width={[1, 1 / 3]} mt="2" mb="2">
+          <Text as="h3">Limit strategy advantage over time</Text>
+          <Text>(percentage dollars saved per coin)</Text>
+          <ResponsiveContainer width="100%" height={400}>
+            <LineChart
+              data={savingsPercentages}
+              margin={{ top: 5, right: 5, left: 10, bottom: 5 }}
+            >
+              <XAxis dataKey="name" stroke="#ebebeb" />
+              <Tooltip />
+              <Legend />
+              <CartesianGrid stroke="#333" />
+              <Line
+                type="monotone"
+                dataKey="btcusd"
+                stroke={COLORS.btc}
+                yAxisId={0}
+              />
+              <Line
+                type="monotone"
+                dataKey="ethusd"
+                stroke={COLORS.eth}
+                yAxisId={1}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </Box>
+      </Flex>
     </>
   );
 };
