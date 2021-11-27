91676bee61c7f3647eef8ecbeaf5a9f53d40521b
Sam Hill <samuhill@gmail.com>
2021-11-27 13:40:57 -0500

    more explanatory text

--- src/components/OpenOrders.js
+++ src/components/OpenOrders.js
@@ -88,10 +88,16 @@ export const OpenOrders = () => {
         Open Orders
       </Text>
 
+      <Text textAlign="left" ml="2" mb="2">
+        Hi, I'm an asset-buying robot.
+      </Text>
       <Text textAlign="left" ml="2" mb="4">
-        If the asset's <span style={{color: "green", fontWeight: "bold"}}>price (green line)</span> drops down to any of the{" "}
-        <strong>white dots (open buy orders)</strong>, I buy it. Then, I create new orders based on the
-        new price.
+        Here's the strategy I'm trying right now: if the asset's{" "}
+        <span style={{ color: "green", fontWeight: "bold" }}>
+          price (green line)
+        </span>{" "}
+        drops down to any of the <strong>white dots (open buy orders)</strong>,
+        I buy it. Then, I create new orders based on the new price.
       </Text>
 
       <Flex width={1} flexDirection={["column", "row"]}>
