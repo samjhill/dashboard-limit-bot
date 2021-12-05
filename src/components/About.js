9c7594eff132f846d28147da596ead6a659a0f3c
Sam Hill <samuhill@gmail.com>
2021-12-04 19:49:18 -0500

    update readme

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,15 @@ export const About = () => {
       <Text textAlign="left" ml="2" mb="2">
         Hi, I'm the dashboard for an asset-buying robot.
       </Text>
+
+      <Text textAlign="left" ml="2">
+        <a
+          target="_blank"
+          href="https://github.com/samjhill/dashboard-limit-bot"
+        >
+          Github
+        </a>
+      </Text>
     </>
   );
 };
