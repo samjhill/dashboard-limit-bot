cd6ae6c74c1d05a6724ae1aa91896c99c960731d
Sam Hill <samuhill@gmail.com>
2022-01-21 01:12:30 -0500

    add todo list

--- src/components/About.js
+++ src/components/About.js
@@ -1,37 +1,42 @@
 import { Text, Box } from "rebass";
 import { BuildLog } from "./BuildLog";
+import { Todos } from "./Todos";
 
 export const About = () => {
 
   return (
     <Box ml="2">
-      <Text as="h1" textAlign="left" ml="2" mb="2" mt="4">
-        Buy the Dip Bot
-      </Text>
-      <Text textAlign="left" ml="2" mb="2">
-        Hi, I'm the dashboard for an asset-buying robot.
-      </Text>
+      <Box backgroundColor="#0000001f" maxWidth="800px" p="2">
+        <Text as="h1" textAlign="left" ml="2" mb="4" mt="4">
+          Buy the Dip Bot
+        </Text>
+        <Text textAlign="left" ml="2" mb="2">
+          Hi, I'm the dashboard for an asset-buying robot.
+        </Text>
 
-      <Text as="h4" textAlign="left" ml="2" mb="5">
-        The goal is to get the best price for a given asset by using a limit
-        strategy:
-        <ul>
-          <li>
-            place{" "}
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://en.wikipedia.org/wiki/Order_(exchange)#Limit_order"
-            >
-              limit orders
-            </a>{" "}
-            at several intervals below the current price
-          </li>
-          <li>if an order gets executed or cancelled, start again</li>
-        </ul>
-      </Text>
+        <Text as="h4" textAlign="left" ml="2" mb="5">
+          The goal is to get the best price for a given asset by using a limit
+          strategy:
+          <ul>
+            <li>
+              place{" "}
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://en.wikipedia.org/wiki/Order_(exchange)#Limit_order"
+              >
+                limit orders
+              </a>{" "}
+              at several intervals below the current price
+            </li>
+            <li>if an order gets executed or cancelled, start again</li>
+          </ul>
+        </Text>
+      </Box>
 
       <BuildLog />
+
+      <Todos />
     </Box>
   );
 };
---
+++ src/components/Todos.js
@@ -0,0 +1,31 @@
+import { Box, Text } from "rebass";
+
+const TodoItem = ({ title, text }) => {
+  return (
+    <Box mt="2" mb="2" p="3" backgroundColor="#0000001f" maxWidth="800px">
+      <Text>{title}</Text>
+    </Box>
+  );
+};
+
+export const Todos = () => {
+  const TodoItems = [
+    {
+      title: "Add integration with traditional stock broker",
+    },
+    {
+      title: "Add per-item settings for finer-grain control and customization of limit orders",
+    },
+  ];
+  return (
+    <Box textAlign="left" ml="2" mt="4">
+      <Text as="h2" mb="2">
+        Todos
+      </Text>
+
+      {TodoItems.map((entry) => (
+        <TodoItem {...entry} />
+      ))}
+    </Box>
+  );
+};
